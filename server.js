7ae91a236ad4d38952a9592e8d7fff1584a00093
Antonio Redondo <AntonioRedondo@users.noreply.github.com>
2017-02-25 10:18:26 +0000

    var changed by const

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var connect = require("connect");
-var serveStatic = require("serve-static");
-var open = require("open");
-var url = "http://localhost:3000/docs/index.htm";
+const connect = require("connect");
+const serveStatic = require("serve-static");
+const open = require("open");
+const url = "http://localhost:3000/docs/index.htm";
 connect().use(serveStatic(".")).listen(3000);
 open(url);
 console.info("App started on " + url + "\nThe system default browser should open automatically.");
