9de9c7881a98d3245dfddccf9835de985c2bba8e
AntonioRedondo <hello@antonioredondo.com>
2017-05-13 17:44:58 +0100

    Minor changes.

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const connect = require("connect");
 const serveStatic = require("serve-static");
 const open = require("open");
-const url = "http://localhost:3000/docs/index.htm";
-connect().use(serveStatic(".")).listen(3000);
+
+const url = "http://localhost:3000";
+
+connect().use(serveStatic("docs", { index: "index.htm" })).listen(3000);
 open(url);
+
 console.info("App started on " + url + "\nThe system default browser should open automatically.");
