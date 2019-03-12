e54cb1e16705fe6ce12b5a2f02627936451c56f0
Tony <hello@antonioredondo.com>
2019-03-12 20:01:16 +0000

    New design

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,11 @@ d.notifyWhenLoaded(".intro__cover");
 
 WebFont.load({
 	google: {
-		families: ["Roboto Slab:300,700", "Source Sans Pro:300,900"]
+		families: ["Roboto Slab:300", "Source Sans Pro:300,900"]
+	},
+	typekit: {
+		id: "bebas-neue",
+		api: "//use.edgefonts.net"
 	},
 	active: function() {
 		d.runWhenAllLoaded(init);
