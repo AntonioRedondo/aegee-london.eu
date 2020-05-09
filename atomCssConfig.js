e02f3f047ea37c908d68d8447211cc6389a531e5
Tony <hello@antonioredondo.com>
2020-05-09 15:37:37 +0100

    Building configuration refactored.

---
+++ atomCssConfig.js
@@ -0,0 +1,22 @@
+module.exports = {
+	// Gulp plugin
+	cssDest: "./src/style/z_atoms.scss",
+	acssConfig: {
+		breakPoints: {
+			bi: "@media (min-width: 2000px)",
+			me: "@media (max-width: 1470px)",
+			sm: "@media (max-width: 1100px)",
+			mo: "@media (max-width: 810px)"
+		}
+	},
+	// webpacck loader
+	outfile: "z_atoms.scss",
+	configs: {
+		breakPoints: {
+			mo: "@media (max-width: 810px)",
+			sm: "@media (max-width: 1100px)",
+			me: "@media (max-width: 1470px)",
+			bi: "@media (min-width: 2000px)",
+		}
+	}
+};
