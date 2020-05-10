bf2b102375849e6e4010ba314495a26de7a0ab64
Tony <hello@antonioredondo.com>
2020-05-10 15:04:53 +0100

    Lighthouse feedback addressed.

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ const url = "http://localhost:3000";
 connect().use(serveStatic("docs", { index: "index.htm" })).listen(3000);
 opn(url);
 
-console.info("App started on " + url + "\nThe system default browser should open automatically."); // eslint-disable-line no-console
+console.info("App started on " + url + "\nThe system default browser will open automatically."); // eslint-disable-line no-console
